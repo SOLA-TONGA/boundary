fb21cd796cad59470c81654e88cc2819d656a1af
amcdowell <armcdowell@hotmail.com>
2013-10-02 14:40:26 +1300

    Updated Property Search

--- transfer-objects/src/main/java/org/sola/services/boundary/transferobjects/search/BaUnitSearchResultTO.java
+++ transfer-objects/src/main/java/org/sola/services/boundary/transferobjects/search/BaUnitSearchResultTO.java
@@ -1,43 +1,54 @@
 /**
  * ******************************************************************************************
- * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations (FAO).
- * All rights reserved.
+ * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations
+ * (FAO). All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- *    1. Redistributions of source code must retain the above copyright notice,this list
- *       of conditions and the following disclaimer.
- *    2. Redistributions in binary form must reproduce the above copyright notice,this list
- *       of conditions and the following disclaimer in the documentation and/or other
- *       materials provided with the distribution.
- *    3. Neither the name of FAO nor the names of its contributors may be used to endorse or
- *       promote products derived from this software without specific prior written permission.
+ * 1. Redistributions of source code must retain the above copyright notice,this
+ * list of conditions and the following disclaimer. 2. Redistributions in binary
+ * form must reproduce the above copyright notice,this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3. Neither the name of FAO nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,STRICT LIABILITY,OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT,STRICT LIABILITY,OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  * *********************************************************************************************
  */
 package org.sola.services.boundary.transferobjects.search;
 
+import java.util.Date;
 import org.sola.services.common.contracts.AbstractTO;
 
 public class BaUnitSearchResultTO extends AbstractTO {
+
     private String id;
     private String name;
     private String nameFirstPart;
     private String nameLastPart;
     private String statusCode;
     private String rightholders;
-    
-    public BaUnitSearchResultTO(){
+    private String islandName;
+    private String townName;
+    private Date registeredDate;
+    private String otherRightholders;
+    private String parcelName;
+    private String registryBookRef;
+    private transient String baUnitTypeCode;
+
+    public BaUnitSearchResultTO() {
         super();
     }
 
@@ -88,4 +99,60 @@ public class BaUnitSearchResultTO extends AbstractTO {
     public void setStatusCode(String statusCode) {
         this.statusCode = statusCode;
     }
+
+    public String getIslandName() {
+        return islandName;
+    }
+
+    public void setIslandName(String islandName) {
+        this.islandName = islandName;
+    }
+
+    public String getTownName() {
+        return townName;
+    }
+
+    public void setTownName(String townName) {
+        this.townName = townName;
+    }
+
+    public Date getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(Date registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+
+    public String getOtherRightholders() {
+        return otherRightholders;
+    }
+
+    public void setOtherRightholders(String otherRightholders) {
+        this.otherRightholders = otherRightholders;
+    }
+
+    public String getParcelName() {
+        return parcelName;
+    }
+
+    public void setParcelName(String parcelName) {
+        this.parcelName = parcelName;
+    }
+
+    public String getRegistryBookRef() {
+        return registryBookRef;
+    }
+
+    public void setRegistryBookRef(String registryBookRef) {
+        this.registryBookRef = registryBookRef;
+    }
+
+    public String getBaUnitTypeCode() {
+        return baUnitTypeCode;
+    }
+
+    public void setBaUnitTypeCode(String baUnitTypeCode) {
+        this.baUnitTypeCode = baUnitTypeCode;
+    } 
 }
